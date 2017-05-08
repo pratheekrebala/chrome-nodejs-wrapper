e1fe5f2bb602606f39d98e332aa85c1a33578bc0
Pratheek Rebala <pratheekrebala@gmail.com>
2017-05-07 20:35:23 -0400

    +init script & ready to go container
    Signed-off-by: Pratheek Rebala <pratheekrebala@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,6 @@ RUN mkdir /chromedata
 RUN apt-get install -y curl
 RUN curl -sL https://deb.nodesource.com/setup_7.x | bash -     && apt-get install -y nodejs
 RUN apt-get install -y x11vnc
-
-CMD ['init.sh']
+COPY . /usr/src/app
+RUN chmod +x /usr/src/app/init.sh
+ENTRYPOINT ["/usr/src/app/init.sh"]
--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 Xvfb :99 -screen 0 1024x768x16 &> xvfb.log &
 DISPLAY=:99.0
 export DISPLAY
 service dbus start
-google-chrome --disable-gpu --remote-debugging-port=9222 --no-sandbox --user-data-dir=/chromedata
+nohup google-chrome --disable-gpu --remote-debugging-port=9222 --no-sandbox --user-data-dir=/chromedata & \n
+x11vnc -nap -noxdamage -passwd PASSWORD -display :99 -forever -rfbport 5901 -shared
