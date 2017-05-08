7eb3dd50c0d8341668a11b8cf79d6c3b28056599
Pratheek Rebala <pratheekrebala@gmail.com>
2017-05-07 20:35:51 -0400

    Start chrome in kiosk mode
    Signed-off-by: Pratheek Rebala <pratheekrebala@gmail.com>

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ Xvfb :99 -screen 0 1024x768x16 &> xvfb.log &
 DISPLAY=:99.0
 export DISPLAY
 service dbus start
-nohup google-chrome --disable-gpu --remote-debugging-port=9222 --no-sandbox --user-data-dir=/chromedata & \n
+nohup google-chrome --disable-gpu --remote-debugging-port=9222 --no-sandbox --kiosk --user-data-dir=/chromedata & \n
 x11vnc -nap -noxdamage -passwd PASSWORD -display :99 -forever -rfbport 5901 -shared
